fea8c9eeec10c7eae77666c4a1a05baf0a69f02a
kimgura <oli999@naver.com>
2023-06-30 12:38:13 +0900

    Spring04 에 Cafe 댓글 기능 구현중...

--- Spring04_Final/src/main/java/com/gura/spring04/cafe/controller/CafeController.java
+++ Spring04_Final/src/main/java/com/gura/spring04/cafe/controller/CafeController.java
@@ -15,6 +15,16 @@ public class CafeController {
 	@Autowired
 	private CafeService service;
 	
+	//새로운 댓글 저장 요청 처리
+	@RequestMapping("/cafe/comment_insert")
+	public String commentInsert(HttpServletRequest request, int ref_group) {
+		//새로운 댓글을 저장하는 로직을 수행한다.
+		service.saveComment(request);
+		//ref_group 은 원글의 글번호이기 때문에 원글 자세히 보기로 다시 리다일렉트 이동된다.
+		return "redirect:/cafe/detail?num="+ref_group;
+	}	
+	
+	
 	@RequestMapping("/cafe/list")
 	public String list(HttpServletRequest request) {
 		//서비스에 HttpServletRequest 객체를 전달해서 응답에 필요한 데이타가 담기도록 하고 
--- Spring04_Final/src/main/java/com/gura/spring04/cafe/service/CafeServiceImpl.java
+++ Spring04_Final/src/main/java/com/gura/spring04/cafe/service/CafeServiceImpl.java
@@ -21,7 +21,7 @@ public class CafeServiceImpl implements CafeService{
 	private CafeDao cafeDao;
 	
 	@Autowired
-	private CafeCommentDao commentDao;
+	private CafeCommentDao cafeCommentDao;
 	
 	//페이징 처리, 검색어 기능을 고려한 비즈니스 로직 처리를 하는 메소드 
 	@Override
@@ -154,11 +154,39 @@ public class CafeServiceImpl implements CafeService{
 		//특수기호를 인코딩한 키워드를 미리 준비한다. 
 		String encodedK=URLEncoder.encode(keyword);
 		
+		/*
+			[ 댓글 페이징 처리에 관련된 로직 ]
+		 */
+		//한 페이지에 몇개씩 표시할 것인지
+		final int PAGE_ROW_COUNT=10;
+		//detail.jsp 페이지에서는 항상 1페이지의 댓글 내용만 출력한다. 
+		int pageNum=1;
+		//보여줄 페이지의 시작 ROWNUM
+		int startRowNum=1+(pageNum-1)*PAGE_ROW_COUNT;
+		//보여줄 페이지의 끝 ROWNUM
+		int endRowNum=pageNum*PAGE_ROW_COUNT;
+		//원글의 글번호를 이용해서 해당글에 달린 댓글 목록을 얻어온다.
+		CafeCommentDto commentDto=new CafeCommentDto();
+		commentDto.setRef_group(num);
+		//1페이지에 해당하는 startRowNum 과 endRowNum 을 dto 에 담아서  
+		commentDto.setStartRowNum(startRowNum);
+		commentDto.setEndRowNum(endRowNum);
+		//1페이지에 해당하는 댓글 목록만 select 되도록 한다. 
+		List<CafeCommentDto> commentList=cafeCommentDao.getList(commentDto);	
+		
+		//원글의 글번호를 이용해서 댓글 전체의 갯수를 얻어낸다.
+		int totalRow=cafeCommentDao.getCount(num);
+		//댓글 전체 페이지의 갯수
+		int totalPageCount=(int)Math.ceil(totalRow/(double)PAGE_ROW_COUNT);
+		
 		//request scope 에 글 하나의 정보 담기
 		request.setAttribute("dto", resultDto);
 		request.setAttribute("condition", condition);
 		request.setAttribute("keyword", keyword);
 		request.setAttribute("encodedK", encodedK);
+		request.setAttribute("totalRow", totalRow);
+		request.setAttribute("commentList", commentList);
+		request.setAttribute("totalPageCount", totalPageCount);
 	}
 
 	@Override
@@ -198,8 +226,39 @@ public class CafeServiceImpl implements CafeService{
 
 	@Override
 	public void saveComment(HttpServletRequest request) {
-		// TODO Auto-generated method stub
+		//폼 전송되는 파라미터 추출 
+		int ref_group=Integer.parseInt(request.getParameter("ref_group")); //원글의 글번호
+		String target_id=request.getParameter("target_id"); //댓글 대상자의 아이디
+		String content=request.getParameter("content"); //댓글의 내용 
+		/*
+		 *  원글의 댓글은 comment_group 번호가 전송이 안되고
+		 *  댓글의 댓글은 comment_group 번호가 전송이 된다.
+		 *  따라서 null 여부를 조사하면 원글의 댓글인지 댓글의 댓글인지 판단할수 있다. 
+		 */
+		String comment_group=request.getParameter("comment_group");
+
+		//댓글 작성자는 session 영역에서 얻어내기
+		String writer=(String)request.getSession().getAttribute("id");
+		//댓글의 시퀀스 번호 미리 얻어내기
+		int seq=cafeCommentDao.getSequence();
 		
+		//저장할 댓글의 정보를 dto 에 담기
+		CafeCommentDto dto=new CafeCommentDto();
+		dto.setNum(seq);
+		dto.setWriter(writer);
+		dto.setTarget_id(target_id);
+		dto.setContent(content);
+		dto.setRef_group(ref_group);
+		//원글의 댓글인경우
+		if(comment_group == null){
+			//댓글의 글번호를 comment_group 번호로 사용한다.
+			dto.setComment_group(seq);
+		}else{
+			//전송된 comment_group 번호를 숫자로 바꾸서 dto 에 넣어준다. 
+			dto.setComment_group(Integer.parseInt(comment_group));
+		}
+		//댓글 정보를 DB 에 저장하기
+		cafeCommentDao.insert(dto);	
 	}
 
 	@Override
