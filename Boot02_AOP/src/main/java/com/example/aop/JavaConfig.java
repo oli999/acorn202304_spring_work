f9b56ce201f1dd21c83f10d4662d307f8df0f074
kimgura <oli999@naver.com>
2023-07-10 16:23:04 +0900

    Boot02_AOP 프로젝트 추가함

---
+++ Boot02_AOP/src/main/java/com/example/aop/JavaConfig.java
@@ -0,0 +1,36 @@
+package com.example.aop;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import com.example.aop.aspect.MessengerAspect;
+import com.example.aop.util.Messenger;
+
+@Configuration
+@EnableAspectJAutoProxy  
+public class JavaConfig {
+	
+	//Messenger 객체를 bean 으로 만들기 
+	@Bean
+	public Messenger myMessenger() {
+		return new Messenger();
+	}
+	
+	//MessengerAspect 를 bean 으로 만들기
+	@Bean
+	public MessengerAspect msa() {
+		return new MessengerAspect();
+	}	
+}
+
+
+
+
+
+
+
+
+
+
+
