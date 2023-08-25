d5a514f1758571cdf4e4d078bc8d585d170f39af
kimgura <oli999@naver.com>
2023-08-25 14:46:19 +0900

    Boot07 에 react js 연동기능 추가함

--- Boot07_Final/src/main/java/com/example/boot07/config/WebConfig.java
+++ Boot07_Final/src/main/java/com/example/boot07/config/WebConfig.java
@@ -2,6 +2,8 @@ package com.example.boot07.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -42,6 +44,18 @@ public class WebConfig implements WebMvcConfigurer{
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	/*
+	 *  Cross Origin Resource Sharing  ( CORS ) 설정 
+	 */
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**") // 모든 경로에 대해 CORS 설정을 적용
+        .allowedOrigins("http://localhost:3000") // 모든 오리진 허용, 필요한 경우 특정 오리진을 지정할 수도 있음
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // 허용할 HTTP 메서드 지정
+        .allowedHeaders("*") // 모든 헤더 허용
+        .allowCredentials(true) // 인증 정보 허용
+        .maxAge(3600); // preflight 요청 결과를 캐시하는 시간 (1시간)
 	}
 }
 
