c5e952f989415e3ec496cee3604d74f817d073e6
kimgura <oli999@naver.com>
2023-09-01 09:30:08 +0900

    Boot08_React 프로젝트 추가함

---
+++ Boot08_React/src/main/java/com/example/boot08/Boot08ReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.boot08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot08ReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot08ReactApplication.class, args);
+	}
+
+}
